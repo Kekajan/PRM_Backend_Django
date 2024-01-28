1b1175e24134228f9d7e2141ca6d06c5d16cae85
Kekajan <kekajanpk4220@gmail.com>
2024-01-29 01:04:50 +0530

    authViews.py created

--- prm/models.py
+++ prm/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Project(models.Model):
@@ -9,18 +10,19 @@ class Project(models.Model):
     desc = models.CharField(max_length=1000)
 
 
-class User(models.Model):
-    firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
-    userName = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=150)
-    userRole = models.CharField(max_length=25)
+# class User(models.Model):
+#     firstName = models.CharField(max_length=100)
+#     lastName = models.CharField(max_length=100)
+#     email = models.EmailField(max_length=100, unique=True)
+#     userName = models.CharField(max_length=100, unique=True)
+#     password = models.CharField(max_length=150)
+#     userRole = models.CharField(max_length=25)
 
-
+ 
 class Task(models.Model):
     taskName = models.CharField(max_length=600)
     description = models.CharField(max_length=2500, null=True)
     member = models.CharField(max_length=255)
     deadline = models.DateField()
     status = models.CharField(max_length=255, default='Todo')
+    projectid = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
